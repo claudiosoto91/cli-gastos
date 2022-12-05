52ad2a705e782085a9f9bb8c0b718ee609525352
Claudio <claudio91soto@gmail.com>
2022-12-05 01:29:51 -0300

    Subiendo tarea

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 
 const fs = require('fs');
+let prompt = require('prompt');
 
 console.log('CLI de gastos | Nucba');
 
-fs.writeFile('./data/gastos.txt', 'Generando .txt de gastos', (error, data) => {
+const listaGastos = [150,200,400,660,100]
+
+const sumarGastos = (listaGastos) =>{
+  fs.readFile('./data/gastos.txt', (error, data) =>{
+
     if (error) console.log(error);
-    console.log(data);
+
+
+    let gastos = '';
+    let suma = 0;
+      for (let i = 0; i< listaGastos.length ; i++){
+        suma = suma + listaGastos[i];
+        gastos += `\n Gasto Número ${i+1} = ${listaGastos[i]} \n Gastos Actualizados: ${suma}`;
+        fs.writeFile('./data/gastos.txt', gastos ,(error, data) => {
+     })
+      }
+      console.log(data.toString());
 });
+}
+sumarGastos(listaGastos);
+
+
+
+
+
+
+
